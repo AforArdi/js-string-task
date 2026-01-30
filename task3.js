55571ce521f130efcc2458c8afd896eca08ba2fa
AforArdi <binsbro36@gmail.com>
2026-01-30 19:08:11 +0600

    string task 3 updated

--- task3.js
+++ task3.js
@@ -1,15 +1,14 @@
 // Check whether a string contains all the vowels a, e, i, o, u
-
 function checkVowel(str){
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     const lowerStr = str.toLowerCase();
 
-    for (let i = 0; i<vowels.length; i++){
-        if(lowerStr.includes(vowels[i])){
-            return true;
+    for (let i = 0; i < vowels.length; i++){
+        if (!lowerStr.includes(vowels[i])){ // is it mising vowel, fail immediately
+            return false;
         }
-        return false;
     }
+    return true;
 }
 const myStr = 'n elephant is older thn the umbrell';
 console.log(checkVowel(myStr));
